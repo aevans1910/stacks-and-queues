7a832ad76410c6f38f6b564fd6b5277a541db4d9
Ariane Evans <ariane.evans@student.makeschool.com>
2020-04-26 14:50:05 -0700

    [add] linkedqueue code

--- queue.py
+++ queue.py
@@ -21,27 +21,37 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        if self.list.size == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.size
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        Running time: O(1) because in our implementation of the linked list, we 
+        have the tail, so we just make it the tail"""
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        if self.length() == 0:
+            return None
+        return self.list.get_at_index(0)
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        Running time: O(1) since its at the head and we therefore don't need
+        to traverse it"""
+        if self.is_empty():
+            raise ValueError("Queue empty")
+        needed_data = self.front()
+        self.list.delete(needed_data)
+        return needed_data
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
