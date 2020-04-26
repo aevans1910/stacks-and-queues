0ba7f8e4e35126b1c88f50304bc31f3595460ea8
Ariane Evans <ariane.evans@student.makeschool.com>
2020-04-26 15:07:16 -0700

    [add] linkedstack queue

--- stack.py
+++ stack.py
@@ -21,27 +21,38 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        if self.list.size == 0:
+            return True
+        return False
+
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        Running time: O(1) with our implementation of the linked list, we add it
+        to the front with the head"""
+        self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        if self.length() == 0:
+            return None
+        return self.list.get_at_index(0)
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        Running time: O(1) with our implementation of the linked list, we remove it
+        from the front with the head"""
+        if self.is_empty():
+            raise ValueError("Queue empty")
+        needed_data = self.peek()
+        self.list.delete(needed_data)
+        return needed_data
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
