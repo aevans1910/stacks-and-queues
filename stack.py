60c4fc65b2180284289ab0a7eb72a9d5572bba25
Ariane Evans <ariane.evans@student.makeschool.com>
2020-04-26 15:17:06 -0700

    [add] arraystack code

--- stack.py
+++ stack.py
@@ -49,7 +49,7 @@ class LinkedStack(object):
         Running time: O(1) with our implementation of the linked list, we remove it
         from the front with the head"""
         if self.is_empty():
-            raise ValueError("Queue empty")
+            raise ValueError("Stack empty")
         needed_data = self.peek()
         self.list.delete(needed_data)
         return needed_data
@@ -73,30 +73,39 @@ class ArrayStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        if len(self.list) == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        Running time: O(1) because we are not traversing the array to add 
+        something to it"""
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        if self.length() == 0:
+            return None
+        return self.list[self.length()-1]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        Running time: O(1) because we are not traversing the array"""
+        if self.is_empty():
+            raise ValueError("Stack empty")
+        needed_data = self.peek()
+        self.list.pop(self.length()-1)
+        return needed_data
 
 
 # Implement LinkedStack and ArrayStack above, then change the assignment below
 # to use each of your Stack implementations to verify they each pass all tests
-Stack = LinkedStack
-# Stack = ArrayStack
+#Stack = LinkedStack
+Stack = ArrayStack
